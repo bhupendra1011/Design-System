1fb66d85669d9c0fb07187a68e563d8a98b53f8a
Bhupendra Negi <bhupendra1011@gmail.com>
2025-10-03 00:54:09 +0530

    eslint fix for web app

--- apps/issues-app/eslint.config.cjs
+++ apps/issues-app/eslint.config.cjs
@@ -1,5 +1,22 @@
 
+const { resolve } = require("node:path");
+
+const project = resolve(__dirname, "tsconfig.json");
+
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/next.js"],
+  root: true,
+  extends: [
+    require.resolve("@repo/eslint-config/next.js")
+  ],
+  parserOptions: {
+    project,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
 };
