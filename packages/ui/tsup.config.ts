283542fe9ecb61b67db3ce1f8daf044c99711619
Bhupendra Negi <bhupendra1011@gmail.com>
2025-10-29 11:32:32 +0530

    fixed tsup

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig((options) => ({
-  entryPoints: ["src/button.tsx", "src/text.tsx", "src/card.tsx", "src/input.tsx", "src/modal.tsx"],
+export default defineConfig({
+  entry: ["src/button.tsx", "src/text.tsx", "src/card.tsx", "src/input.tsx", "src/modal.tsx"],
   format: ["cjs", "esm"],
   dts: false,
   external: ["react", "@repo/icons"],
   treeshake: true,
-  ...options,
-}));
+  splitting: false,
+  clean: true,
+});
